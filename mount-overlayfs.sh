d653701fd910961b8a0755e1f1a55f05ebfe02c4
glider0 <41747840+glider0@users.noreply.github.com>
2019-09-19 14:39:33 +0800

    mount_overlayfs: remove old upperdir and workdir

--- mount-overlayfs.sh
+++ mount-overlayfs.sh
@@ -2,6 +2,7 @@
 
 OWRT_DIR=${OWRT_DIR:-../openwrt}
 
+rm -rf workdir upperdir
 mkdir -p openwrt workdir upperdir
 
 sudo mount -t overlay overlay -o "lowerdir=${OWRT_DIR},upperdir=upperdir,workdir=workdir" openwrt
